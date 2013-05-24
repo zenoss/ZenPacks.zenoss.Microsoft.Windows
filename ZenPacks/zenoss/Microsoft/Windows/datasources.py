dba1861a31551b6c3c94a26f81dcfca038a310f9
Brian Edwards <bedwards@zenoss.com>
2013-05-24 14:55:29 +0000

    add datasource, IInfo, and Info classes. Add config_key method. Add bindings in configure.zcml

--- ZenPacks/zenoss/Microsoft/Windows/datasources.py
+++ ZenPacks/zenoss/Microsoft/Windows/datasources.py
@@ -8,22 +8,105 @@
 ##############################################################################
 
 import logging
+from pprint import pformat
 from twisted.internet import defer
+from Products.Zuul.form import schema
+from Products.Zuul.infos import ProxyProperty
+from Products.Zuul.interfaces import IRRDDataSourceInfo
+from Products.Zuul.utils import ZuulMessageFactory as _t
+from Products.Zuul.infos.template import RRDDataSourceInfo
 from ZenPacks.zenoss.PythonCollector.datasources.PythonDataSource \
-    import PythonDataSourcePlugin
+    import PythonDataSource, PythonDataSourcePlugin
 from .txwinrm.util import ConnectionInfo
 from .txwinrm.shell import create_single_shot_command
 
 log = logging.getLogger("zen.MicrosoftWindows")
+_ZENPACKID = 'ZenPacks.zenoss.Microsoft.Windows'
 
 
-class SingleShotPlugin(PythonDataSourcePlugin):
+class SingleCounterDataSource(PythonDataSource):
+    """
+    Subclass PythonDataSource to put a
+    new datasources into Zenoss
+    Base datasource for TypeperfSc1DataSource and
+    PowershellGetCounterDataSource
+    """
+
+    ZENPACKID = _ZENPACKID
+    component = '${here/id}'
+    cycletime = 300
+    counter = ''
+    _properties = PythonDataSource._properties + (
+        {'id': 'counter', 'type': 'string'})
+
+
+class TypeperfSc1DataSource(SingleCounterDataSource):
+    """
+    Datasource used to capture datapoints from winrs typeperf -sc1.
+    """
+
+    sourcetypes = ('WinRS typeperf -sc1',)
+    sourcetype = sourcetypes[0]
+    plugin_classname = _ZENPACKID + '.datasources.TypeperfSc1Plugin'
+
+
+class PowershellGetCounterDataSource(SingleCounterDataSource):
+    """
+    Datasource used to capture datapoints from winrs powershell get-counter.
+    """
+
+    sourcetypes = ('WinRS powershell Get-Counter',)
+    sourcetype = sourcetypes[0]
+    plugin_classname = _ZENPACKID + '.datasources.PowershellGetCounterPlugin'
+
+
+class ITypeperfSc1Info(IRRDDataSourceInfo):
+    """
+    Provide the UI information for the WinRS typeperf -sc1 datasource.
+    """
+
+    namespace = schema.TextLine(
+        group=_t(TypeperfSc1DataSource.sourcetype),
+        title=_t('Counter'))
+
+
+class IPowershellGetCounterInfo(IRRDDataSourceInfo):
+    """
+    Provide the UI information for the WinRS powershell Get-Counter datasource.
+    """
+
+    namespace = schema.TextLine(
+        group=_t(PowershellGetCounterDataSource.sourcetype),
+        title=_t('Counter'))
+
+
+class SingleCounterInfo(RRDDataSourceInfo):
+    """
+    Pull in proxy values so they can be utilized within the WinRS typeperf -sc1
+    plugin.
+    """
+
+    testable = False
+    cycletime = ProxyProperty('cycletime')
+    counter = ProxyProperty('counter')
+
+
+class SingleCounterPlugin(PythonDataSourcePlugin):
 
     proxy_attributes = ('zWinUser', 'zWinPassword')
 
+    @classmethod
+    def config_key(cls, datasource, context):
+        """
+        Uniquely pull in datasources
+        """
+        return (context.device().id,
+                datasource.getCycleTime(context),
+                datasource.counter)
+
     @defer.inlineCallbacks
     def collect(self, config):
-        log.debug('TypeperfSc1Plugin collect {0}'.format(config))
+        log.warn('BME- SingleCounterPlugin collect {0}'.format(config))
         scheme = 'http'
         port = 5985
         results = []
@@ -45,7 +128,8 @@ class SingleShotPlugin(PythonDataSourcePlugin):
         defer.returnValue(results)
 
     def onSuccess(self, results, config):
-        log.debug('TypeperfPlugin onSuccess {0} {1}'.format(results, config))
+        log.warn('BME- SingleCounterPlugin onSuccess {0} {1}'
+                 .format(results, config))
         data = self.new_data()
         self._parse_results(results, data)
         data['events'].append(dict(
@@ -67,25 +151,33 @@ class SingleShotPlugin(PythonDataSourcePlugin):
         return data
 
 
-class TypeperfSc1Plugin(SingleShotPlugin):
+class TypeperfSc1Plugin(SingleCounterPlugin):
 
     def _build_command_line(self, counter):
-        'typeperf "{0}" -sc 1'.format(counter)
+        log.warn('BME- TypeperfSc1Plugin _build_command_line {0}'
+                 .format(counter))
+        return 'typeperf "{0}" -sc 1'.format(counter)
 
     def _parse_results(self, results, data):
+        log.warn('BME- TypeperfSc1Plugin _parse_results {0} {1}'
+                 .format(pformat(results), pformat(data)))
         for ds, result in results:
             timestamp, value = result.split(',')
             data['values'][ds.component][ds.datasource] = (value, timestamp)
             pass
 
 
-class PowershellGetCounterPlugin(SingleShotPlugin):
+class PowershellGetCounterPlugin(SingleCounterPlugin):
 
     def _build_command_line(self, counter):
+        log.warn('BME- PowershellGetCounterPlugin _build_command_line {0}'
+                 .format(counter))
         return "powershell -NoLogo -NonInteractive -NoProfile -OutputFormat " \
                "XML -Command \"get-counter -counter '{0}'\"".format(counter)
 
     def _parse_results(self, results, data):
+        log.warn('BME- PowershellGetCounterPlugin _parse_results {0} {1}'
+                 .format(pformat(results), pformat(data)))
         for datasource, result in results:
             # data['values']
             pass
