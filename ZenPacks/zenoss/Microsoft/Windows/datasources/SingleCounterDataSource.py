1c0b553d9cde2d700523877028ebc0511084e7ee
Brian Edwards <bedwards@zenoss.com>
2013-05-31 16:13:00 +0000

    add choice for powershell get-counter strategy

---
+++ ZenPacks/zenoss/Microsoft/Windows/datasources/SingleCounterDataSource.py
@@ -0,0 +1,206 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+"""
+A datasource that uses WinRS to run typeperf -sc1 or powershell Get-Counter.
+
+See Products.ZenModel.ZenPack._getClassesByPath() to understand how this class
+gets discovered as a datasource type in Zenoss.
+"""
+
+import csv
+import time
+import logging
+import calendar
+from zope.component import adapts
+from zope.interface import implements
+from zope.schema.vocabulary import SimpleVocabulary
+from twisted.internet import defer
+from Products.Zuul.form import schema
+from Products.Zuul.infos import ProxyProperty
+from Products.Zuul.interfaces import IRRDDataSourceInfo
+from Products.Zuul.utils import ZuulMessageFactory as _t
+from Products.Zuul.infos.template import RRDDataSourceInfo
+from ZenPacks.zenoss.PythonCollector.datasources.PythonDataSource \
+    import PythonDataSource, PythonDataSourcePlugin
+
+from ZenPacks.zenoss.Microsoft.Windows.utils \
+    import addLocalLibPath
+
+addLocalLibPath()
+
+from txwinrm.util import ConnectionInfo
+from txwinrm.shell import create_single_shot_command
+
+log = logging.getLogger("zen.MicrosoftWindows")
+ZENPACKID = 'ZenPacks.zenoss.Microsoft.Windows'
+SINGLE_COUNTER_SOURCETYPE = 'WinRS Single Counter'
+TYPEPERF_STRATEGY = 'typeperf -sc1'
+POWERSHELL_STRATEGY = 'powershell Get-Counter'
+
+
+class SingleCounterDataSource(PythonDataSource):
+    """
+    Subclass PythonDataSource to put a new datasources into Zenoss
+    """
+
+    ZENPACKID = ZENPACKID
+    component = '${here/id}'
+    cycletime = 300
+    counter = ''
+    strategy = ''
+    _properties = PythonDataSource._properties + (
+        {'id': 'counter', 'type': 'string'},
+        {'id': 'strategy', 'type': 'string'},
+        )
+    sourcetypes = (SINGLE_COUNTER_SOURCETYPE,)
+    sourcetype = sourcetypes[0]
+    plugin_classname = ZENPACKID + \
+        '.datasources.SingleCounterDataSource.SingleCounterPlugin'
+
+
+class ISingleCounterInfo(IRRDDataSourceInfo):
+    """
+    Provide the UI information for the WinRS Single Counter datasource.
+    """
+
+    counter = schema.TextLine(
+        group=_t(SINGLE_COUNTER_SOURCETYPE),
+        title=_t('Counter'))
+
+    strategy = schema.Choice(
+        group=_t(SINGLE_COUNTER_SOURCETYPE),
+        title=_t('Strategy'),
+        default=TYPEPERF_STRATEGY,
+        vocabulary=SimpleVocabulary.fromValues(
+            [TYPEPERF_STRATEGY, POWERSHELL_STRATEGY]),)
+
+
+class SingleCounterInfo(RRDDataSourceInfo):
+    """
+    Pull in proxy values so they can be utilized within the WinRS Single
+    Counter plugin.
+    """
+    implements(ISingleCounterInfo)
+    adapts(SingleCounterDataSource)
+
+    testable = False
+    cycletime = ProxyProperty('cycletime')
+    counter = ProxyProperty('counter')
+    strategy = ProxyProperty('strategy')
+
+
+class TypeperfSc1Strategy(object):
+
+    def build_command_line(self, counter):
+        return 'typeperf "{0}" -sc 1'.format(counter)
+
+    def parse_results(self, results, data):
+        for ds, result in results:
+            if result.exit_code != 0:
+                log.info('Non-zero exit code ({0}) for counter, {1}, on {2}'
+                         .format(
+                         result.exit_code, ds.params['counter'], ds.device))
+                continue
+            rows = list(csv.reader(result.stdout))
+            timestamp_str, value_str = rows[1]
+            format = '%m/%d/%Y %H:%M:%S.%f'
+            timestamp = calendar.timegm(time.strptime(timestamp_str, format))
+            value = float(value_str)
+            data['values'][ds.component][ds.datasource] = (value, timestamp)
+
+
+typeperf_strategy = TypeperfSc1Strategy()
+
+
+class PowershellGetCounterStrategy(object):
+
+    def build_command_line(self, counter):
+        return "powershell -NoLogo -NonInteractive -NoProfile -OutputFormat " \
+               "XML -Command \"get-counter -counter '{0}'\"".format(counter)
+
+    def parse_results(self, results, data):
+        for ds, result in results:
+            if result.exit_code != 0:
+                log.info('Non-zero exit code ({0}) for counter, {1}, on {2}'
+                         .format(
+                         result.exit_code, ds.params['counter'], ds.device))
+                continue
+            log.warn("BME- {0}".format(result.stdout))
+            # data['values'][ds.component][ds.datasource] = (value, timestamp)
+
+
+powershell_strategy = PowershellGetCounterStrategy()
+
+
+class SingleCounterPlugin(PythonDataSourcePlugin):
+
+    proxy_attributes = ('zWinUser', 'zWinPassword')
+
+    @classmethod
+    def config_key(cls, datasource, context):
+        """
+        Uniquely pull in datasources
+        """
+        return (context.device().id,
+                datasource.getCycleTime(context),
+                datasource.counter,
+                datasource.strategy)
+
+    @classmethod
+    def params(cls, datasource, context):
+        return dict(counter=datasource.talesEval(datasource.counter, context),
+                    strategy=datasource.strategy)
+
+    @defer.inlineCallbacks
+    def collect(self, config):
+        scheme = 'http'
+        port = 5985
+        results = []
+        for datasource in config.datasources:
+            auth_type = 'basic'
+            if '@' in datasource.zWinUser:
+                auth_type = 'kerberos'
+            conn_info = ConnectionInfo(
+                datasource.manageIp,
+                auth_type,
+                datasource.zWinUser,
+                datasource.zWinPassword,
+                scheme,
+                port)
+            cmd = create_single_shot_command(conn_info)
+            strategy = typeperf_strategy
+            if datasource.strategy == POWERSHELL_STRATEGY:
+                strategy = powershell_strategy
+            command_line = strategy.build_command_line(
+                datasource.params['counter'])
+            result = yield cmd.run_command(command_line)
+            results.append((datasource, result))
+        defer.returnValue(results)
+
+    def onSuccess(self, results, config):
+        data = self.new_data()
+        self._parse_results(results, data)
+        data['events'].append(dict(
+            eventClassKey='winrsCollectionSuccess',
+            eventKey='winrsCollection',
+            summary='winrs: successful collection',
+            device=config.id))
+        return data
+
+    def onError(self, result, config):
+        msg = 'winrs: failed collection {0} {1}'.format(result, config)
+        log.error(msg)
+        data = self.new_data()
+        data['events'].append(dict(
+            eventClassKey='winrsCollectionError',
+            eventKey='winrsCollection',
+            summary=msg,
+            device=config.id))
+        return data
