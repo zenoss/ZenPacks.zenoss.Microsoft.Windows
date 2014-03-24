e58608bc95f826139dc64135c6b6771377894e91
Chet Luther <chet.luther@gmail.com>
2014-03-24 15:21:05 -0500

    Version 2.0.3dev -> 2.1.0dev.
    2.0.3 hotfix branch has been created for fixes that should be
    back-ported to the 2.0 release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Microsoft.Windows"
-VERSION = "2.0.3dev"
+VERSION = "2.1.0dev"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.Microsoft']
