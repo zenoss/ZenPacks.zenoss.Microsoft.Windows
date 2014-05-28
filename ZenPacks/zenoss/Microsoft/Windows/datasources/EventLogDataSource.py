d6bb4b3b0f2e3cc9c1c380393ffa626e746f3cbf
ssbunyk <tbunyk@softserveinc.com>
2014-05-28 13:43:45 +0300

    removed old datasource

--- ZenPacks/zenoss/Microsoft/Windows/datasources/EventLogDataSource.py
+++ ZenPacks/zenoss/Microsoft/Windows/datasources/EventLogDataSource.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2014, all rights reserved.
 #
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
@@ -8,10 +8,12 @@
 ##############################################################################
 
 """
-A datasource that uses WinRS to collect Windows Event Logs
+A datasource that uses Powershell comandlet to collect Windows Event Logs
 
 """
+
 import logging
+import json
 
 from twisted.internet import defer
 
@@ -30,28 +32,13 @@ from ZenPacks.zenoss.PythonCollector.datasources.PythonDataSource \
 from ..txwinrm_utils import ConnectionInfoProperties, createConnectionInfo
 
 # Requires that txwinrm_utils is already imported.
-from txwinrm.subscribe import create_event_subscription
+from txwinrm.shell import create_single_shot_command
+
 
 log = logging.getLogger("zen.MicrosoftWindows")
 ZENPACKID = 'ZenPacks.zenoss.Microsoft.Windows'
 
-subscriptions_dct = {}
-
-
-def string_to_lines(string):
-    if isinstance(string, (list, tuple)):
-        return string
-    elif hasattr(string, 'splitlines'):
-        return string.splitlines()
-
-    return None
-
-
 class EventLogDataSource(PythonDataSource):
-    """
-    Subclass PythonDataSource to put a new datasources into Zenoss
-    """
-
     ZENPACKID = ZENPACKID
     component = '${here/id}'
     cycletime = 300
@@ -68,31 +55,25 @@ class EventLogDataSource(PythonDataSource):
     _properties = PythonDataSource._properties + (
         {'id': 'eventlog', 'type': 'string'},
         {'id': 'query', 'type': 'lines'},
-        )
+    )
 
 
 class IEventLogInfo(IRRDDataSourceInfo):
-    """
-    Provide the UI information for the WinRS Single Counter datasource.
-    """
     cycletime = schema.TextLine(
-        title=_t(u'Cycle Time (seconds)'))
-
+        title=_t(u'Cycle Time (seconds)')
+    )
     eventlog = schema.TextLine(
         group=_t('WindowsEventLog'),
-        title=_t('Event Log'))
-
+        title=_t('Event Log')
+    )
     query = schema.Text(
         group=_t(u'WindowsEventLog'),
         title=_t('Event Query'),
-        xtype='twocolumntextarea')
+        xtype='twocolumntextarea'
+    )
 
 
 class EventLogInfo(RRDDataSourceInfo):
-    """
-    Pull in proxy values so they can be utilized within the WinRS Single
-    Counter plugin.
-    """
     implements(IEventLogInfo)
     adapts(EventLogDataSource)
 
@@ -102,10 +83,17 @@ class EventLogInfo(RRDDataSourceInfo):
     query = ProxyProperty('query')
 
 
+def string_to_lines(string):
+    if isinstance(string, (list, tuple)):
+        return string
+    elif hasattr(string, 'splitlines'):
+        return string.splitlines()
+
+    return None
+
+
 class EventLogPlugin(PythonDataSourcePlugin):
     proxy_attributes = ConnectionInfoProperties
-
-    subscriptionID = {}
 
     @classmethod
     def config_key(cls, datasource, context):
@@ -117,86 +105,63 @@ class EventLogPlugin(PythonDataSourcePlugin):
             datasource.plugin_classname,
             params.get('eventlog'),
             params.get('query'),
-            )
+        )
 
     @classmethod
     def params(cls, datasource, context):
-        params = {}
-
-        params['eventlog'] = datasource.talesEval(
-            datasource.eventlog, context)
-
-        params['query'] = datasource.talesEval(
-            ' '.join(string_to_lines(datasource.query)), context)
-
-        return params
+        return dict(
+            eventlog=datasource.talesEval(
+                datasource.eventlog, context
+            ), 
+            query=datasource.talesEval(
+                ' '.join(string_to_lines(datasource.query)),
+                context
+            )
+        )
 
     @defer.inlineCallbacks
     def collect(self, config):
         results = []
-
         log.info('Start Collection of Events')
 
         ds0 = config.datasources[0]
         conn_info = createConnectionInfo(ds0)
 
-        path = ds0.params['eventlog']
+        query = EventLogQuery(conn_info)
+
+        eventlog = ds0.params['eventlog']
         select = ds0.params['query']
-        try:
-            subscription = subscriptions_dct[(ds0.manageIp, path)]
-        except:
-            subscription = create_event_subscription(conn_info)
-            yield subscription.subscribe(path, select)
-            subscriptions_dct[(ds0.manageIp, path)] = subscription
 
-        def log_event(event):
-            results.append(event)
-        yield subscription.pull_once(log_event)
+        res = yield query.run(eventlog, select)
+        defer.returnValue(json.loads(
+            '\n'.join(res.stdout)
+        ))
 
-        defer.returnValue(results)
 
     def onSuccess(self, results, config):
         data = self.new_data()
         for evt in results:
+            severity = {
+                'Information': ZenEventClasses.Clear,
+                'Warning': ZenEventClasses.Warning,
+                'Error': ZenEventClasses.Critical,
+            }.get(evt['severity'], ZenEventClasses.Info)
 
-            if evt.rendering_info is not None:
-                """
-                evt.system.time_created
-                evt.system.channel
-                evt.system.event_id
-                evt.system.provider
-                evt.rendering_info.keywords
-                evt.rendering_info.message
-                """
-                errlevel = evt.rendering_info.level
-
-                evtmessage = "EventID: {evtid}\nSource: {evtsource}\nLog: {evtlog}\nMessage: {message}".format(
-                    evtid=evt.system.event_id,
-                    evtsource=evt.system.provider,
-                    evtlog=evt.system.channel,
-                    message=evt.rendering_info.message)
-
-                severity = {
-                    'Information': ZenEventClasses.Clear,
-                    'Warning': ZenEventClasses.Warning,
-                    'Error': ZenEventClasses.Critical,
-                    }.get(errlevel, ZenEventClasses.Info)
-
-                data['events'].append({
-                    'eventClassKey': 'WindowsEventLog',
-                    'eventKey': 'WindowsEvent',
-                    'severity': severity,
-                    'summary': 'Collected Event: %s' % evtmessage,
-                    'device': config.id,
-                    })
+            data['events'].append({
+                'eventClassKey': 'WindowsEventLog',
+                'eventKey': 'WindowsEvent',
+                'severity': severity,
+                'summary': 'Collected Event: %s' % evt['message'],
+                'device': config.id,
+            })
 
         data['events'].append({
             'device': config.id,
             'summary': 'Windows EventLog: successful event collection',
-            'severity': ZenEventClasses.Info,
+            'severity': ZenEventClasses.Clear,
             'eventKey': 'WindowsEventCollection',
             'eventClassKey': 'WindowsEventLogSuccess',
-            })
+        })
 
         return data
 
@@ -209,5 +174,61 @@ class EventLogPlugin(PythonDataSourcePlugin):
             'eventClassKey': 'WindowsEventCollectionError',
             'eventKey': 'WindowsEventCollection',
             'summary': msg,
-            'device': config.id})
+            'device': config.id
+        })
         return data
+
+
+class EventLogQuery(object):
+    def __init__(self, conn_info):
+        self.winrs = create_single_shot_command(conn_info)
+
+    PS_COMMAND = "powershell -NoLogo -NonInteractive -NoProfile " \
+            "-OutputFormat TEXT -Command "
+
+    PS_SCRIPT = r'''
+        $FormatEnumerationLimit = -1;
+        $Host.UI.RawUI.BufferSize = New-Object Management.Automation.Host.Size(4096, 25);
+
+        function get_new_recent_entries($logname, $selector={$True}) { 
+            <# create HKLM:\SOFTWARE\zenoss\logs if not exists #>
+            New-Item HKLM:\SOFTWARE\zenoss -ErrorAction SilentlyContinue;
+            New-Item HKLM:\SOFTWARE\zenoss\logs -ErrorAction SilentlyContinue;
+            
+            <# check the time of last read log entry #>
+            $last_read = Get-ItemProperty -Path HKLM:\SOFTWARE\zenoss\logs -Name $logname -ErrorAction SilentlyContinue;
+            
+            <# If last log entry was older that 24 hours - read only for last 24 hours #>
+            [DateTime]$yesterday = (Get-Date).AddHours(-24);
+            [DateTime]$after = $yesterday;
+            if ($last_read) {
+                $last_read = [DateTime]$last_read.$logname;
+                if ($last_read -gt $yesterday) {
+                    $after = $last_read;
+                };
+            };
+            
+            <# Fetch events #>
+            $events = Get-EventLog -After $after -LogName $logname;
+            
+            if($events) { <# update the time of last read log entry #>
+                [DateTime]$last_read = (@($events)[0]).TimeGenerated; 
+                Set-Itemproperty -Path HKLM:\SOFTWARE\zenoss\logs -Name $logname -Value ([String]$last_read);
+            }
+            
+            '[' + (($events | ? $selector | %% { "{
+                `"severity`": `"$($_.EntryType)`",
+                `"message`": `"Collected Event: EventID: $($_.EventID)\nSource: $($_.Source)\nMessage: $($_.Message)`"
+            }" }) -join ', ') + ']'
+        };
+        get_new_recent_entries %s %s;
+    '''
+
+    def run(self, eventlog, selector):
+        command = "{0} \"& {{{1}}}\"".format(
+            self.PS_COMMAND,
+            self.PS_SCRIPT.replace('\n', ' ').replace('"', r'\"') % (eventlog, selector)
+        )
+        return self.winrs.run_command(command)
+
+
