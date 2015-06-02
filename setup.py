81e0b19202696dcae7e7d6e0e8f3c529298f1ae3
Chet Luther <chet.luther@gmail.com>
2015-06-02 16:36:30 -0500

    Start 2.4.5 hotfix: Version 2.4.4 -> 2.4.5dev.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Microsoft.Windows"
-VERSION = "2.4.4"
+VERSION = "2.4.5dev"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.Microsoft']
