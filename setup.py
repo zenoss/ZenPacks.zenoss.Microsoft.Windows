1dcdc06ac0d316880e6447702c6790c6e37feafa
Dave Bouchillon <dbouchillon@zenoss.com>
2015-08-18 15:24:25 -0500

    post hotfix release:  Version 2.4.6dev -> 2.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Microsoft.Windows"
-VERSION = "2.4.6"
+VERSION = "2.5.0dev"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.Microsoft']
