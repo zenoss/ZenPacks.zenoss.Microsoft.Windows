b6b05122c77e3c4de580e482b73a95a8e91aea8d
Vadim A. Byshovets <vbyshovets@zenoss.com>
2015-08-27 14:14:25 +0300

    ZEN-18831: added pickled data helper

---
+++ ZenPacks/zenoss/Microsoft/Windows/tests/utils.py
@@ -0,0 +1,20 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2015, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+import os
+import pickle
+
+
+def load_pickle(self, filename):
+    with open(os.path.join(
+            os.path.dirname(__file__),
+            'data',
+            self.__class__.__name__,
+            filename), 'rb') as f:
+        return pickle.load(f)
