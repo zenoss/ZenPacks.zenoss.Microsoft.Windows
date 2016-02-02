7d66d307e5d3f96929a03a158da8f98b1961a4c7
Dave Bouchillon <dbouchillon@zenoss.com>
2016-02-02 09:32:12 -0600

    spit out err results so we know what to look for in future

--- ZenPacks/zenoss/Microsoft/Windows/datasources/ShellDataSource.py
+++ ZenPacks/zenoss/Microsoft/Windows/datasources/ShellDataSource.py
@@ -318,6 +318,7 @@ class CustomCommandStrategy(object):
         # Give error feedback to user
         eventClass = dsconf.eventClass if dsconf.eventClass else "/Status"
         if result.stderr:
+            log.debug(result.stderr)
             try:
                 err_index = [i for i, val in enumerate(result.stderr) if "At line:" in val][0]
                 msg = 'Custom Command error: ' + ''.join(result.stderr[:err_index])
