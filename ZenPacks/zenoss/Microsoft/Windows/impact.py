c08ccbec7ffcfed9e2414a624b2e8d8d360fab2a
Joseph Anderson <j053ph4j@gmail.com>
2016-06-07 11:52:15 -0500

    -added impact.py for backwards compatibility with MS Exchange ZP

---
+++ ZenPacks/zenoss/Microsoft/Windows/impact.py
@@ -0,0 +1,23 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2016, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+try:
+    from ZenPacks.zenoss.Impact.impactd.relations import ImpactEdge, DSVRelationshipProvider, RelationshipEdgeError
+    from ZenPacks.zenoss.Impact.impactd.interfaces import IRelationshipDataProvider
+except ImportError:
+    IMPACT_INSTALLED = False
+else:
+    IMPACT_INSTALLED = True
+
+
+if IMPACT_INSTALLED:
+    from ZenPacks.zenoss.Microsoft.Windows.zenpacklib import ImpactRelationshipDataProvider
+
+    class DeviceRelationsProvider(ImpactRelationshipDataProvider):
+        '''RelationsProvider stub for backwards compatibility'''
