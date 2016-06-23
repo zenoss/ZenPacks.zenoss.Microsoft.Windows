4dea94deda8a7b361ce2a10456f76c6a5ee465b1
Dave Bouchillon <dbouchillon@zenoss.com>
2016-06-23 17:11:01 -0500

    Readme updates for Windows ZP
    add ignore case to regex compile
    
    Change details text to show Manually instead of User

--- ZenPacks/zenoss/Microsoft/Windows/interfaces.py
+++ ZenPacks/zenoss/Microsoft/Windows/interfaces.py
@@ -43,5 +43,5 @@ class IWinServiceInfo(zuul.IWinServiceInfo):
     Info adapter for WinService components.
     """
     formatted_description = form_schema.TextLine(title=_t(u'Description'), readonly=True)
-    usermonitor = form_schema.Bool(title=_t(u'User Selected Monitor State'),
+    usermonitor = form_schema.Bool(title=_t(u'Manually Selected Monitor State'),
                                    alwaysEditable=True)
