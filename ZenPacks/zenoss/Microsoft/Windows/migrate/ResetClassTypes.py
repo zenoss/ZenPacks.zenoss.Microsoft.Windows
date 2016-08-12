zenoss/ZenPacks.zenoss.Microsoft.Windows

commit 68a2b76d13beeafe64b6f84db30a27620f1f21e1

Fix multiple "Interfaces" component types
We were getting into a situation where all device.os.interfaces() were
of the WindowsInterface type, but due to having meta_type instance
values shadowing the WindowsInterface class-scoped meta_type value, some
had a meta_type of Interface, and some had a meta_type of IpInterface.
The meta_type split caused a split in how the UI represented them.

This change causes the instance-scoped meta_type and portal_type
properties to be deleted, preventing them from shadowing the
class-scoped values. Any objects where the resulting meta_type no longer
matches the indexed meta_type value are also reindexed.

Partial fixes ZEN-24401.

ZenPacks/zenoss/Microsoft/Windows/migrate/ResetClassTypes.py
@@ -7,14 +7,11 @@
 #
 ##############################################################################
 
-"""
-
-Reset meta_type and portal_types for objects
-
-"""
+"""Reset meta_type and portal_type for objects that need it."""
 
 # Logging
 import logging
+LOG = logging.getLogger("zen.Microsoft.Windows.migrate.{}".format(__name__))
 
 # Zenoss Imports
 from zope.event import notify
@@ -24,60 +21,113 @@ from Products.Zuul.catalog.events import IndexingEvent
 from Products.Zuul.interfaces import ICatalogTool
 
 # ZenPack Imports
-from ZenPacks.zenoss.Microsoft.Windows import schema
-from ZenPacks.zenoss.Microsoft.Windows.WinService import WinService
-from ZenPacks.zenoss.Microsoft.Windows.CPU import CPU
+from ZenPacks.zenoss.Microsoft.Windows import progresslog
 from ZenPacks.zenoss.Microsoft.Windows.ClusterDisk import ClusterDisk
 from ZenPacks.zenoss.Microsoft.Windows.ClusterInterface import ClusterInterface
 from ZenPacks.zenoss.Microsoft.Windows.ClusterNetwork import ClusterNetwork
 from ZenPacks.zenoss.Microsoft.Windows.ClusterNode import ClusterNode
 from ZenPacks.zenoss.Microsoft.Windows.ClusterResource import ClusterResource
 from ZenPacks.zenoss.Microsoft.Windows.ClusterService import ClusterService
+from ZenPacks.zenoss.Microsoft.Windows.CPU import CPU
 from ZenPacks.zenoss.Microsoft.Windows.FileSystem import FileSystem
 from ZenPacks.zenoss.Microsoft.Windows.Interface import Interface
 from ZenPacks.zenoss.Microsoft.Windows.OSProcess import OSProcess
 from ZenPacks.zenoss.Microsoft.Windows.TeamInterface import TeamInterface
 from ZenPacks.zenoss.Microsoft.Windows.WinIIS import WinIIS
+from ZenPacks.zenoss.Microsoft.Windows.WinService import WinService
 from ZenPacks.zenoss.Microsoft.Windows.WinSQLBackup import WinSQLBackup
 from ZenPacks.zenoss.Microsoft.Windows.WinSQLDatabase import WinSQLDatabase
 from ZenPacks.zenoss.Microsoft.Windows.WinSQLInstance import WinSQLInstance
 from ZenPacks.zenoss.Microsoft.Windows.WinSQLJob import WinSQLJob
 
-LOG = logging.getLogger('zen.MicrosoftWindows')
+
+# If the migration takes longer than this interval, a running progress
+# showing elapsed and estimated remaining time will be logged this
+# often. The value is in seconds.
+PROGRESS_LOG_INTERVAL = 10
 
 
 class ResetClassTypes(ZenPackMigration):
-    version = Version(2, 6, 0)
+    version = Version(2, 6, 3)
 
     def migrate(self, pack):
-        LOG.info('Resetting component attributes')
-        catalog = ICatalogTool(pack.getDmdRoot('Devices'))
+        LOG.info("searching for objects")
+        results = ICatalogTool(pack.getDmdRoot("Devices")).search(types=[
+            ClusterDisk,
+            ClusterInterface,
+            ClusterNetwork,
+            ClusterNode,
+            ClusterResource,
+            ClusterService,
+            CPU,
+            FileSystem,
+            Interface,
+            OSProcess,
+            TeamInterface,
+            WinIIS,
+            WinService,
+            WinSQLBackup,
+            WinSQLDatabase,
+            WinSQLInstance,
+            WinSQLJob,
+            ])
 
-        klasses = [CPU, ClusterDisk, ClusterInterface, 
-                   ClusterNetwork, ClusterNode, ClusterResource, 
-                   ClusterService, FileSystem, Interface, 
-                   OSProcess, TeamInterface, WinIIS, 
-                   WinSQLBackup, WinSQLDatabase, WinSQLInstance,
-                   WinSQLJob]
-        for klass in klasses:
-            self.reset_class(catalog, klass)
-
-    def reset_class(self, catalog, klass):
-        '''reset portal_type and meta_type to class name'''
-        name = klass.__name__
-        results = catalog.search(klass)
         if not results.total:
+            LOG.info("no objects to migrate")
             return
 
-        LOG.info("Indexing %s %s objects" % (results.total, name))
-        for result in results:
-            try:
-                ob = result.getObject()
-                ob.meta_type = name 
-                ob.portal_type = name
-            except Exception as e:
-                log.warn('problem setting to "%s"' % (name))
-                continue
+        LOG.info("starting: %s total objects", results.total)
+        progress = progresslog.ProgressLogger(
+            LOG,
+            prefix="progress",
+            total=results.total,
+            interval=PROGRESS_LOG_INTERVAL)
 
-            ob.index_object()
-            notify(IndexingEvent(ob))
+        objects_migrated = 0
+
+        for result in results:
+            if self.migrate_result(result):
+                objects_migrated += 1
+
+            progress.increment()
+
+        LOG.info(
+            "finished: %s of %s objects required migration",
+            objects_migrated,
+            results.total)
+
+    def migrate_result(self, result):
+        """Return True if result needed to be migrated.
+
+        Delete instance properties that shadow class properties, and reindex
+        the object if its resulting meta_type no longer matches its indexed
+        meta_type.
+
+        """
+        try:
+            obj = result.getObject()
+        except Exception:
+            return False
+
+        migrated = False
+
+        try:
+            del(obj.meta_type)
+        except Exception:
+            pass
+        else:
+            migrated = True
+
+        try:
+            del(obj.portal_type)
+        except Exception:
+            pass
+        else:
+            migrated = True
+
+        if result.meta_type != obj.meta_type:
+            obj.index_object()
+            notify(IndexingEvent(obj))
+            migrated = True
+
+        return migrated

README.mediawiki
@@ -1231,6 +1231,7 @@ Installing this ZenPack will add the following items to your Zenoss system.
 ; 2.6.3
 * Fix potential "clusternetworks" and "clusternodes" errors after upgrading (ZEN-24401)
 * Fix AttributeError: serviceclass on Windows Services after v515 update (ZEN-24347)
+* Fix duplicated "Interfaces" components after upgrade (ZEN-24401)
 
 ; 2.6.2
 * Fix WinRM ZenPack - Windows Services page elections conflict with WinService template exclusions (ZEN-24165)

commit a61bcb38baa5a5d3cc293bba299c2d995dc42655

Omit WinService from ResetClassTypes migration
The RemoveWinRMServices migration script that's also targeted at 2.6.3
removes all WinRMSerice instances that this migration script was
reindexing. Due to ZenPackCmd performing all migrations within the same
paused(onIndexingEvent) context manager this lead to a situation where
the deferred indexing if WinRMService instances would fail with the
following traceback.

    ...
      File "/opt/zenoss/Products/Zuul/catalog/global_catalog.py", line 287, in searchExcerpt
        o.name(), o.device().titleOrId())
    AttributeError: 'NoneType' object has no attribute 'titleOrId'

This bug was introduced in 68a2b76. I thought the omision of WinService
indexing was accidental, so I added it. In retrospect the omission was
necessary.

The simple solution is to stop reindexing WinRMService instances that
are going to be removed anyway.

Fixes ZEN-24436.

ZenPacks/zenoss/Microsoft/Windows/migrate/ResetClassTypes.py
@@ -34,7 +34,6 @@ from ZenPacks.zenoss.Microsoft.Windows.Interface import Interface
 from ZenPacks.zenoss.Microsoft.Windows.OSProcess import OSProcess
 from ZenPacks.zenoss.Microsoft.Windows.TeamInterface import TeamInterface
 from ZenPacks.zenoss.Microsoft.Windows.WinIIS import WinIIS
-from ZenPacks.zenoss.Microsoft.Windows.WinService import WinService
 from ZenPacks.zenoss.Microsoft.Windows.WinSQLBackup import WinSQLBackup
 from ZenPacks.zenoss.Microsoft.Windows.WinSQLDatabase import WinSQLDatabase
 from ZenPacks.zenoss.Microsoft.Windows.WinSQLInstance import WinSQLInstance
@@ -65,7 +64,6 @@ class ResetClassTypes(ZenPackMigration):
             OSProcess,
             TeamInterface,
             WinIIS,
-            WinService,
             WinSQLBackup,
             WinSQLDatabase,
             WinSQLInstance,