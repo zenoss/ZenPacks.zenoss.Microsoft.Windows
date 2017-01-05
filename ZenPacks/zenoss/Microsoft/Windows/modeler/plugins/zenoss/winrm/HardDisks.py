4d143380df0d7efb0d31ce5b75bc3168d9a99941
Dave Bouchillon <dbouchillon@zenoss.com>
2017-01-05 17:00:48 -0600

    Add support for Hard Disks in Windows
    Fixes ZPS-479
    
    This adds the Hard Disk component and modeler plugin to the ZenPack.
    Models Hard Disk and finds associated file systems.  Links to File
    System and provides a reverse link back to the Hard Disk.
    
    TODO: Add Monitoring template
    
    Fix according to review

---
+++ ZenPacks/zenoss/Microsoft/Windows/modeler/plugins/zenoss/winrm/HardDisks.py
@@ -0,0 +1,120 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2013, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+'''
+Windows Hard Disks
+
+Models hard disks by querying Win32_Disk via WMI.
+'''
+
+from ZenPacks.zenoss.Microsoft.Windows.modeler.WinRMPlugin import WinRMPlugin
+from ZenPacks.zenoss.Microsoft.Windows.utils import save
+
+
+class HardDisks(WinRMPlugin):
+    compname = 'hw'
+    relname = 'harddisks'
+    modname = 'ZenPacks.zenoss.Microsoft.Windows.HardDisk'
+
+    associators = {
+        'diskdrives': {
+            'seed_class': 'Win32_DiskDrive',
+            'associations': [{'return_class': 'Win32_DiskDriveToDiskPartition',
+                              'search_class': 'Win32_DiskDrive',
+                              'search_property': 'DeviceID',
+                              'where_type': 'AssocClass'},
+                             {'return_class': 'Win32_LogicalDiskToPartition',
+                              'search_class': 'Win32_DiskPartition',
+                              'search_property': 'DeviceID',
+                              'where_type': 'AssocClass'
+                              }]
+        }
+    }
+
+    @save
+    def process(self, device, results, log):
+        rm = self.relMap()
+        try:
+            diskdrives = results.get('diskdrives').get('Win32_DiskDrive')
+        except Exception:
+            return rm
+        if not diskdrives:
+            return rm
+        partitions = results.get('diskdrives').get('Win32_DiskDriveToDiskPartition')
+        volumes = results.get('diskdrives').get('Win32_LogicalDiskToPartition')
+        for drive in diskdrives:
+            utilization = 0
+            fs_ids = []
+            for partition in partitions[drive.DeviceID]:
+                utilization += int(partition.Size)
+                for volume in volumes[partition.DeviceID]:
+                    fs_ids.append(self.prepId(volume.DeviceID))
+            freespace = int(drive.Size) - utilization
+            if freespace < 0:
+                freespace = 0
+            rm.append(self.objectMap({
+                'id': self.prepId(drive.PNPDeviceID),
+                'title': drive.Caption,
+                'size': int(drive.Size),
+                'partitions': int(drive.Partitions),
+                'capabilities': drive.CapabilityDescriptions,
+                'serialNumber': drive.SerialNumber.strip(),
+                'freespace': freespace,
+                'disk_ids': make_disk_ids(drive),
+                'fs_ids': fs_ids,
+            }))
+        return rm
+
+
+def reorder_serial(input_serial):
+    """
+    Take a serial number and reorder it into the orginal.
+
+    This will exclude the first 2 digits from the original format
+    since those are lost on the Windows side.
+    String must have even number of characters.
+    """
+    size = len(input_serial)
+    if size % 2 != 0:
+        return
+
+    # Remove the first 2 characters since not unique
+    input_serial = input_serial[2:]
+    new_serial = ''
+    for i in range(size, 0, -2):
+        new_serial += input_serial[i - 2:i]
+
+    return new_serial
+
+
+def make_disk_ids(disk_drive):
+    """From a Win32_DiskDrive object, create the disk_ids list."""
+    disk_ids = []
+    disk_ids.append(disk_drive.DeviceID)
+
+    # If no PNPDeviceID, its not a physical disk, so return None
+    pnpDevice = disk_drive.PNPDeviceID.strip()
+    if not pnpDevice:
+        return
+
+    disk_ids.append(pnpDevice)
+
+    # Append SerialNumber
+    serial = disk_drive.SerialNumber.strip()
+    if not serial:
+        return disk_ids
+
+    disk_ids.append(serial)
+
+    # Append the reordered serial to match CiscoUCS, minus first 2 characters
+    size = len(serial)
+    if size == 32:
+        disk_ids.append(reorder_serial(serial))
+
+    return disk_ids
