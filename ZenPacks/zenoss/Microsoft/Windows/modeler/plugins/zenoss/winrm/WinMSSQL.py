ea5a8309e31d3a2dd49b41b63a0d6c744c840e59
Dave Bouchillon <dbouchillon@zenoss.com>
2017-03-02 11:07:39 -0600

    Make sure buffer_size is list

--- ZenPacks/zenoss/Microsoft/Windows/modeler/plugins/zenoss/winrm/WinMSSQL.py
+++ ZenPacks/zenoss/Microsoft/Windows/modeler/plugins/zenoss/winrm/WinMSSQL.py
@@ -326,8 +326,8 @@ class WinMSSQL(WinRMPlugin):
             job_sqlConnection.append("write-host 'username:'$job.OwnerLoginName;}}")
             job_sqlConnection.append("catch { continue; }")
 
-            buffer_size = ('$Host.UI.RawUI.BufferSize = New-Object '
-                           'Management.Automation.Host.Size (4096, 512);')
+            buffer_size = ['$Host.UI.RawUI.BufferSize = New-Object '
+                           'Management.Automation.Host.Size (4096, 512);']
 
             instance_info = yield winrs.run_command(
                 ''.join(buffer_size + getSQLAssembly(int(om_instance.sql_server_version.split('.')[0])) +
