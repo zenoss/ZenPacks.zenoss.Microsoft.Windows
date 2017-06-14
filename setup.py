227c191a64109ac02b6abb42c9cafb953225ae93
addjuarez <addjuarez@yahoo.com>
2017-06-14 10:49:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 ################################
-# These variables are overwritten by Zenoss when the ZenPack 123
+# These variables are overwritten by Zenoss when the ZenPack 12
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Microsoft.Windows"
