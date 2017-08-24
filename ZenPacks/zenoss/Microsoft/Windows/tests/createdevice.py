3c2cdb9f72bbb873925554ffc17786bab65a0786
Dave Bouchillon <dbouchillon@zenoss.com>
2017-08-24 15:36:21 -0500

    update createdevice to create multiple devices with multiple services
    used to test ZPS-1783

--- ZenPacks/zenoss/Microsoft/Windows/tests/createdevice.py
+++ ZenPacks/zenoss/Microsoft/Windows/tests/createdevice.py
@@ -10,6 +10,7 @@
 import logging
 import functools
 import importlib
+from ZODB.transact import transact
 
 from Products.ZenRelations.RelationshipBase import RelationshipBase
 from Products.ZenRelations.ToManyContRelationship import ToManyContRelationship
@@ -81,7 +82,21 @@ def addNonContained(object, relname, target):
     return target
 
 
-def create_device(dmd, device_name):
+def create_devices(dmd, commit=False, device_name_root='windows{}', ip_root='10.10.10.{}', num_devices=10, num_services=100):
+    for x in xrange(num_devices):
+        ip = ip_root.format(x)
+        if commit is False:
+            create_device(dmd, device_name_root.format(x), ip, num_services)
+        else:
+            create_device_commit(dmd, device_name_root.format(x), ip, num_services)
+
+
+@transact
+def create_device_commit(dmd, device_name, ip='10.10.10.10', num_services=1):
+    create_device(dmd, device_name, ip, num_services)
+
+
+def create_device(dmd, device_name, ip='10.10.10.10', num_services=1):
     '''
     Return a Windows Device suitable for Impact functional testing.
     '''
@@ -92,7 +107,7 @@ def create_device(dmd, device_name):
     windows1 = dc.createInstance(device_name)
 
     windows1.domain_controller = True
-    windows1.manageIp = '10.10.10.10'
+    windows1.manageIp = ip
     windows1.msexchangeversion = 'MSExchangeIS'
     windows1.clusterdevices = ['cluster1']
 
@@ -126,7 +141,15 @@ def create_device(dmd, device_name):
 
     addContained(windows1.os, 'winrmiis', WinIIS('iis1'))
 
-    addContained(windows1.os, 'winservices', WinService('ws1'))
+    for x in xrange(num_services):
+        svc = WinService('ws{}'.format(x + 1))
+        if x % 2 == 0:
+            svc.startMode = 'Auto'
+        elif x % 3 == 0:
+            svc.startMode = 'Disabled'
+        else:
+            svc.startMode = 'Manual'
+        addContained(windows1.os, 'winservices', svc)
 
     instance1 = addContained(windows1.os, 'winsqlinstances', WinSQLInstance('instance1'))
     addContained(instance1, 'backups', WinSQLBackup('backup1'))
@@ -138,7 +161,7 @@ def create_device(dmd, device_name):
     return windows1
 
 
-def create_cluster_device(dmd, device_name):
+def create_cluster_device(dmd, device_name, ip='10.10.20.10'):
     '''
     Return a Windows Cluster Device suitable for analytics bundle create script.
     '''
@@ -148,7 +171,7 @@ def create_cluster_device(dmd, device_name):
     # Endpoint
     cluster1 = dc.createInstance(device_name)
     cluster1.domain_controller = True
-    cluster1.manageIp = '10.10.10.10'
+    cluster1.manageIp = ip
     cluster1.msexchangeversion = 'MSExchangeIS'
     cluster1.clusterdevices = ['node1']
 
