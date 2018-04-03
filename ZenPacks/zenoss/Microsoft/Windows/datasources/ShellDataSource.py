c741cd49198f25a5d7687147496e54b449ca5bb6
Dave Bouchillon <dbouchillon@zenoss.com>
2018-04-03 17:11:13 -0500

    Apply simple obfuscation of sql password in queries
    Fixes ZPS-3302
    
    For now, apply a simple b64 encoding to obfuscate the password sent into the sql scripts for modeling/monitoring.

--- ZenPacks/zenoss/Microsoft/Windows/datasources/ShellDataSource.py
+++ ZenPacks/zenoss/Microsoft/Windows/datasources/ShellDataSource.py
@@ -20,6 +20,7 @@ import urllib
 from urlparse import urlparse
 from traceback import format_exc
 import re
+import base64
 
 from zope.component import adapts
 from zope.component import getGlobalSiteManager
@@ -435,9 +436,12 @@ class SqlConnection(object):
         self.version = version
 
         # DB Connection Object
+        pwd = base64.b64encode(sqlpassword)
+        self.sqlConnection.append("$x = '{}';".format(pwd))
+        self.sqlConnection.append("$p = [System.Text.Encoding]::ASCII.GetString([Convert]::FromBase64String($x));")
         self.sqlConnection.append("$con = new-object "
                                   "('Microsoft.SqlServer.Management.Common.ServerConnection')"
-                                  "'{}', '{}', '{}';".format(instance, sqlusername, sqlpassword))
+                                  '"{}", "{}", "$p";'.format(instance, sqlusername, sqlpassword))
 
         if login_as_user:
             # Login using windows credentials
@@ -445,7 +449,7 @@ class SqlConnection(object):
             self.sqlConnection.append("$con.ConnectAsUser=$true;")
             # Omit domain part of username
             self.sqlConnection.append("$con.ConnectAsUserName='{0}';".format(sqlusername.split("\\")[-1]))
-            self.sqlConnection.append("$con.ConnectAsUserPassword='{0}';".format(sqlpassword))
+            self.sqlConnection.append("$con.ConnectAsUserPassword=$p;")
         else:
             self.sqlConnection.append("$con.Connect();")
 
@@ -487,7 +491,8 @@ class PowershellMSSQLStrategy(object):
                                       "('{{db_name}}', $db_name).replace('{{status}}', $db.Status);}")
         counters_sqlConnection.append("$query = 'select RTRIM(instance_name), "
                                       "RTRIM(counter_name), RTRIM(cntr_value) from "
-                                      "sys.dm_os_performance_counters';")
+                                      "sys.dm_os_performance_counters where instance_name in "
+                                      "(select name from sys.databases)';")
         counters_sqlConnection.append("$ds = $dbMaster.ExecuteWithResults($query);")
         counters_sqlConnection.append("if($ds.Tables[0].rows.count -gt 0) {$ds.Tables[0].rows"
                                       "| % {write-host $_.Column1':counter:'$_.Column2':value:'$_.Column3;} } }")
