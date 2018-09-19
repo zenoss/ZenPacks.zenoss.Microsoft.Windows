fce0b294fe1e84c62784883381646121e0312190
Dave Bouchillon <dbouchillon@zenoss.com>
2018-09-19 15:13:42 -0500

    Begin Hotfix: Version 2.9.2 -> 2.9.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Microsoft.Windows"
-VERSION = "2.9.2"
+VERSION = "2.9.3"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.Microsoft']
