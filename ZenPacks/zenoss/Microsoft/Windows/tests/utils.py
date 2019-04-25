f64a8faf64873f8810fd1b74e345ca4b38b93aad
Dave Bouchillon <dbouchillon@zenoss.com>
2019-04-25 15:10:33 -0500

    check to be sure CommandLine exists, and not None
    Fixes ZPS-5676
    
    also write unit tests to cover this situation

--- ZenPacks/zenoss/Microsoft/Windows/tests/utils.py
+++ ZenPacks/zenoss/Microsoft/Windows/tests/utils.py
@@ -12,6 +12,27 @@ import gzip
 import pickle
 
 from Products.DataCollector.ApplyDataMap import ApplyDataMap
+from txwinrm.enumerate import ItemsAccumulator
+
+
+class ItemBuilder(object):
+
+    def get_items(self, accumulator, item_dict):
+        if isinstance(item_dict, list):
+            for element in item_dict:
+                self.get_items(accumulator, element)
+            return
+        accumulator.new_item()
+        for k, v in item_dict.iteritems():
+            accumulator.add_property(k, v)
+
+    def create_results(self, items_dict):
+        items = {}
+        for k, v in items_dict.iteritems():
+            accumulator = ItemsAccumulator()
+            self.get_items(accumulator, v)
+            items[k] = accumulator.items
+        return items
 
 
 class StringAttributeObject(dict):
