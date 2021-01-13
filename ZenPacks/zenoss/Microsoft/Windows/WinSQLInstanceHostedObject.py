6b30ef6d3cfe52d4b7586a8b00b23e332eb2b51c
Oleksandr Trach <56258953+otrach@users.noreply.github.com>
2021-01-13 11:02:01 +0200

    Add Availability Group and Replica components (#1135)
    * Add Availability Group and Replica components
    
    Implements ZPS-7435.
    
    * Add Availability group component.
    * Add monitoring, incremental modeling and events for Availability group component.
    * Add WinSQLInstance(winsqlavailabilitygroups) 1:M (winsqlinstance)WinSQLAvailabilityGroup relation.
    * Add Availability replica component.
    * Add non-containing realtion to WinSQLInstance and containing relation to WinSQLAvailabilityGroup.
    
    * Minor fixes
    
    Implements ZPS-7435.

---
+++ ZenPacks/zenoss/Microsoft/Windows/WinSQLInstanceHostedObject.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2020, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+from . import schema
+
+
+class WinSQLInstanceHostedObject(schema.WinSQLInstanceHostedObject):
+    """
+    Base class for components, which are hosted on SQL Instance.
+    """
+
+    def get_sql_instance(self):
+        """
+        Returns SQL Instance on which Availability Replicas' Primary Replica is placed. TODO: revise this
+        """
+        sql_instance = None
+
+        winsqlinstance_relation = getattr(self, 'winsqlinstance', None)
+        if winsqlinstance_relation:
+            sql_instance = winsqlinstance_relation()
+
+        return sql_instance
+
+    @property
+    def cluster_node_server(self):
+        hosted_sql_instance = self.get_sql_instance()
+        if hosted_sql_instance:
+            return getattr(hosted_sql_instance, 'cluster_node_server', '')
+
+    @property
+    def perfmon_instance(self):
+        hosted_sql_instance = self.get_sql_instance()
+        if hosted_sql_instance:
+            return getattr(hosted_sql_instance, 'perfmon_instance', '')
+
+    @property
+    def owner_node_ip(self):
+        hosted_sql_instance = self.get_sql_instance()
+        if hosted_sql_instance:
+            return getattr(hosted_sql_instance, 'owner_node_ip', '')
+
+    @property
+    def instancename(self):
+        hosted_sql_instance = self.get_sql_instance()
+        if hosted_sql_instance:
+            return getattr(hosted_sql_instance, 'instancename', '')
